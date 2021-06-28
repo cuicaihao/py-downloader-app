7b29b936bce6a537ded37f83349fff14ec507f29
Chris (Caihao) Cui <cuicaihao@users.noreply.github.com>
2021-06-28 17:42:48 +1000

    ✅ update test.py

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-from src.pySpeedDownloader import pydownloader
+# Step 1: install the package from PyPi by
+# pip install pySpeedDownloader
+
+# Step 2: run this scripts
+# python test.py
+
+from pySpeedDownloader import pydownloader
 
 url = "https://github.com/tensorflow/tensorflow/archive/refs/heads/master.zip"
 file_name = "tensorflow-master.zip"
